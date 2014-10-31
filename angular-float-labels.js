b7b9793fbfdd962cde851fba978b60003f9d233b
Chris Roberson <chrisronline@gmail.com>
2014-10-31 15:12:03 -0400

    Fixing #1 and #2

--- angular-float-labels.js
+++ angular-float-labels.js
@@ -2,27 +2,39 @@
   'use strict';
 
   angular.module('angular-float-labels', [])
-    .directive('placeholder', function() {
+    .directive('placeholder', function($parse) {
       return {
         restrict: 'A',
-        link: function($scope, $element, $attrs) {
+        require: 'ngModel',
+        link: function($scope, $element, $attrs, $ngModel) {
           var wrapper = $('<span class="angular-float-labels-wrapper"></span>');
           var label = $('<label class="angular-float-labels-label"></label').html($attrs.placeholder);
-          var index = $element.index();
-          var parent = $element.parent();
+          var element = $element;
+          var input = $element;
+          var parent = element.parent();
+          var index = element.index();
+
+          var selectize = _.has($attrs, 'selectize');
+          var selectizeInstance;
+          if (selectize) {
+            selectizeInstance = element.data('selectize') || element.selectize;
+            element = element.parent().find('.selectize-control').first();
+            input = element.find('.selectize-input input').first();
+            parent = element.find('.selectize-input').first();
+          }
 
           function focus() { label.addClass('focused'); }
           function blur() { label.removeClass('focused'); }
           function showLabel() { label.addClass('toggled'); }
           function hideLabel() { label.removeClass('toggled'); }
           function change(e) {
-            $element.val().length || (!$(this)[0].checkValidity() && !$(this).val().length)
+            input.val().length || (!$(this)[0].checkValidity() && !$(this).val().length)
               ? showLabel()
               : hideLabel();
           }
           function init() {
-            $element.addClass('angular-float-labels-element');
-            wrapper.append(label).append($element);
+            input.addClass('angular-float-labels-element');
+            wrapper.append(label).append(input);
             if (index === 0) {
               parent.prepend(wrapper);
             }
@@ -30,16 +42,44 @@
               wrapper.insertAfter(parent.children()[index - 1]);
             }
 
-            $element.on('input change', change);
-            $element.on('focus', focus);
-            $element.on('blur', blur);
+            input.on('input change', change);
+            input.on('focus', focus);
+            input.on('blur', blur);
             $scope.$on('$destroy', function() {
-              $element.off('input change', change);
-              $element.off('focus', focus);
-              $element.off('blur', blur);
+              input.off('input change', change);
+              input.off('focus', focus);
+              input.off('blur', blur);
             });
           }
-          init();
+          function initSelectize() {
+            function selectizeToggle(value) {
+              value && value.length ? showLabel() : hideLabel();
+            }
+            function selectizeToggleItems() {
+              selectizeToggle(selectizeInstance.items);
+            }
+            function selectizeBlur() {
+              blur();
+              selectizeToggleItems();
+            }
+            function selectizeFocus() {
+              focus();
+              selectizeToggleItems();
+            }
+            parent.addClass('angular-float-labels-element');
+            parent.before(label);
+            selectizeInstance.on('type', selectizeToggle);
+            selectizeInstance.on('dropdown_open', selectizeFocus);
+            selectizeInstance.on('dropdown_close', selectizeBlur);
+            selectizeInstance.on('blur', selectizeBlur);
+            selectizeInstance.on('change', selectizeToggleItems)
+          }
+          selectize ? initSelectize() : init();
+          // Without this, the label does not appear in Canary
+          setTimeout(function() {
+            var initValue = $parse($attrs.ngModel)($scope);
+            initValue && initValue.length ? showLabel() : hideLabel();
+          });
         }
       };
     });

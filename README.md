f5caedd10567a29db19f0fb2eac8c58f5cab6c6b
Chris Roberson <chrisronline@gmail.com>
2015-06-26 22:24:27 -0500

    Adding support for a custom attribute
    Addressing #3

--- README.md
+++ README.md
@@ -25,6 +25,18 @@ Usage
 Customize
 -------
 
+### Attribute
+If you do not want to the logic to trigger from the `placeholder` attribute, please include `angular-float-labels.custom.js` instead of `angular-float-labels.js` and configure the provider appropriately:
+
+```
+.config(function(floatLabelProvider) {
+		floatLabelProvider.setAttributeName('floatLabel');
+})
+ ```
+
+
+### CSS
+
 Override CSS classes defined within angular-float-labels.css or simply do not include it and create them all yourself.
 
 
@@ -35,4 +47,11 @@ Please view the [demo](http://www.chrisronline.com/angular-float-labels/index.ht
 
 Release Notes
 ---------
+- v0.1.0 - Add support for a custom attribute instead of `placeholder`
+- v0.0.7 - Fix validity check
+- v0.0.6 - see v0.0.5
+- v0.0.5 - Deprecated `ngModel` requirement and removed unnecessary lodash/underscore dependency
+- v0.0.4 - Fixing issue #1 and #2
+- v0.0.3 - Adding main files to bower.json
+- v0.0.2 - Fixing dependency versions
 - v0.0.1 - Basic input support
---
+++ angular-float-labels.custom.js
@@ -0,0 +1,86 @@
+(function() {
+  'use strict';
+
+  function floatLabelDirectiveLink($scope, $element, $attrs, $$parse, $$name) {
+    var label = $('<label class="angular-float-labels-label"></label').html($$name);
+    var element = $element;
+    var input = $element;
+    var parent = element.parent().addClass('angular-float-labels-wrapper');
+    var index = element.index();
+
+    var selectize = $attrs.hasOwnProperty('selectize');
+    var selectizeInstance;
+    if (selectize) {
+      selectizeInstance = element.data('selectize') || element.selectize;
+      element = element.parent().find('.selectize-control').first();
+      input = element.find('.selectize-input input').first();
+      parent = element.find('.selectize-input').first();
+    }
+
+    function focus() { label.addClass('focused'); }
+    function blur() { label.removeClass('focused'); }
+    function showLabel() { label.addClass('toggled'); }
+    function hideLabel() { label.removeClass('toggled'); }
+    function change(e) {
+      input.val().length || (!$(this)[0].checkValidity() && $(this).val().length)
+        ? showLabel()
+        : hideLabel();
+    }
+    function init() {
+      input.addClass('angular-float-labels-element');
+      input.before(label);
+      input.on('input change', change);
+      input.on('focus', focus);
+      input.on('blur', blur);
+      $scope.$on('$destroy', function() {
+        input.off('input change', change);
+        input.off('focus', focus);
+        input.off('blur', blur);
+      });
+    }
+    function initSelectize() {
+      function selectizeToggle(value) {
+        value && value.length ? showLabel() : hideLabel();
+      }
+      function selectizeToggleItems() {
+        selectizeToggle(selectizeInstance.items);
+      }
+      function selectizeBlur() {
+        blur();
+        selectizeToggleItems();
+      }
+      function selectizeFocus() {
+        focus();
+        selectizeToggleItems();
+      }
+      parent.addClass('angular-float-labels-element');
+      parent.before(label);
+      selectizeInstance.on('type', selectizeToggle);
+      selectizeInstance.on('dropdown_open', selectizeFocus);
+      selectizeInstance.on('dropdown_close', selectizeBlur);
+      selectizeInstance.on('blur', selectizeBlur);
+      selectizeInstance.on('change', selectizeToggleItems)
+    }
+    selectize ? initSelectize() : init();
+    // Without this, the label does not appear in Canary
+    setTimeout(function() {
+      var initValue = $$parse($attrs.ngModel)($scope);
+      initValue && initValue.length ? showLabel() : hideLabel();
+    });
+  }
+
+  angular.module('angular-float-labels', [])
+    .provider('floatLabel', function($compileProvider) {
+      this.setAttributeName = function(name) {
+        $compileProvider.directive.apply(null, [name, function($parse) {
+          return {
+            restrict: 'A',
+            link: function($scope, $element, $attrs) {
+              floatLabelDirectiveLink($scope, $element, $attrs, $parse, $attrs[name]);
+            }
+          };
+        }]);
+      };
+      this.$get = function() { };
+    });
+})();
